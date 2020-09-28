9a9e8445cfb4bb23101d55ae9a2b339a972d1cde
anup <anuppanta5555@gmail.com>
2020-09-28 08:34:50 +0545

    datatype lesson repo added

---
+++ data_types/index.js
@@ -0,0 +1,25 @@
+/*let variable_first_name =1, variable_another_name = 'hello world';
+console.log("========");
+
+console.log(variable_first_name);
+console.log(variable_another_name);*/
+
+/*let first_number = 10, decimal_value = 10.5, first_string = 'Hy all', bool_value = true, undefined_value;
+console.log('==============');
+console.log(first_number);
+console.log(decimal_value)
+console.log(first_string);
+console.log(bool_value);
+console.log(undefined_value);
+console.log('==============');*/
+
+let integer_value=1;
+let string_value='hello world'; 
+let bool_value=false; 
+let undefined_value;
+console.log("=======");
+console.log(typeof integer_value);
+console.log(typeof string_value);
+console.log(typeof bool_value);
+console.log(typeof undefined_value);
+console.log("======");
