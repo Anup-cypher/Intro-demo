8c984c30b85d726acca7e536f36324ea505b8159
anup <anuppanta5555@gmail.com>
2020-10-04 07:32:17 +0545

    arry added

---
+++ array/index.js
@@ -0,0 +1,41 @@
+// let fruit1 = 'apple', fruit2 = 'mango', fruit3 = 'pears';
+/*let fruit = ['apple', 'mango', 'pears'];
+let fun = new Array('FB', 'CB', 'VB');
+// console.log(fruit1, fruit2, fruit3);
+console.log(fruit);
+console.log(fun);*/
+
+let fruit = ['apple', 'mango', 'pears'];
+// console.log(fruit);
+
+/*let name = ['Ram', 'Hari', 'Shyam', 'Harry', 1, true, null, [1,2]];
+console.log(name);
+// console.log(typeof name[0]);
+console.log(typeof name);
+console.log(name [7]);
+
+console.log(typeof name[7]);
+
+
+console.log(name [7][0]);
+console.log(name [7] [1]);*/
+// let address = new Array('Kathmandu', 'Pokhara', 'Biratnagar');
+// console.log(address);
+
+// let newArr = [['hello', 2, true], ['iims', 'bscit'],['kathmandu']]
+
+// let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+// console.log(newArr[2][2]);
+// console.log(fruit.length);
+// console.log(fruit.indexOf('pears'));
+// console.log(fruit.indexOf('apple'));
+// // console.log(fruit[2]);
+// fruit[2] = 'avacado';
+// // console.log(fruit[2]);
+// console.log(fruit);
+
+fruit.push(['avacado', 'banana']);
+// fruit[1] = 'guava';
+// console.log(fruit);
+fruit.pop();
+console.log(fruit);
--- data_types/index.js
+++ data_types/index.js
@@ -5,7 +5,14 @@ console.log(variable_first_name);
 console.log(variable_another_name);*/
 
 /*let first_number = 10, decimal_value = 10.5, first_string = 'Hy all', bool_value = true, undefined_value;
-console.log('==============');
+
+console.log(first_number);
+console.log(typeof first_number);
+
+first_number = 'hello';
+console.log(first_number);
+console.log(typeof first_number);*/
+/*console.log('==============');
 console.log(first_number);
 console.log(decimal_value)
 console.log(first_string);
@@ -13,7 +20,7 @@ console.log(bool_value);
 console.log(undefined_value);
 console.log('==============');*/
 
-let integer_value=1;
+/*let integer_value=1;
 let string_value='hello world'; 
 let bool_value=false; 
 let undefined_value;
@@ -22,4 +29,37 @@ console.log(typeof integer_value);
 console.log(typeof string_value);
 console.log(typeof bool_value);
 console.log(typeof undefined_value);
-console.log("======");
+console.log(null)
+console.log("======");*/
+/*
+const CONSTANT_VALUE = 'IIMS';
+CONSTANT_VALUE = 'red';*/
+
+l/*et first_number = 10;
+let second_number = 20;
+let first_string = "Hello World!";
+let first_decimal = 10.10;
+let boolean_value = true;
+let undefined_value;
+let null_value = null;
+const CONSTANT_VALUE = "IIMS COLLEGE";
+
+console.log(" ============= ");
+
+console.log(first_number + ":" + typeof first_number );
+
+console.log(second_number + ":" + typeof second_number);
+
+console.log(first_string + ":" +  typeof first_string);
+
+console.log(first_decimal + ":" + typeof first_decimals);
+
+console.log(boolean_value + ":" + typeof boolean_value);
+
+console.log(undefined_value + ":" + typeof undefined_value);
+
+console.log(null_value + ":" + typeof null_value);
+
+console.log(CONSTANT_VALUE + ":" + typeof CONSTANT_VALUE);
+
+console.log(" ============ ");*/
